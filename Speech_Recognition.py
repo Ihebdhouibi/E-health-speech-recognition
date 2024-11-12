8bca7fe507de34cc16eeeb496c5795546ab919d5
IhebDhouibi <ihebdhouibidh@gmail.com>
2024-11-12 16:35:14 +0100

    Code adjusted for converting audio to text

---
+++ Speech_Recognition.py
@@ -0,0 +1,30 @@
+from os import path
+import speech_recognition as sr
+
+# First try converting microphone voice to text
+#r = sr.Recognizer()
+#with sr.Microphone() as source:
+#    print("You have the stage!")
+#    audio = r.listen(source)
+    
+    # Recognize speech using Sphinx | Offline 
+#    try:
+#        print("Sphinx thinks you said: " + r.recognize_sphinx(audio_data=audio))
+#    except sr.UnknownValueError:
+#        print("Sphinx encoutered an unkown error")
+#    except sr.RequestError as e:
+#        print("Sphinx error: {0}".format(e))
+
+# Converting audio file to text
+audio_file = "data\harvard.wav"
+r = sr.Recognizer()
+with sr.AudioFile(audio_file) as source:
+    audio = r.record(source)
+
+# recognize audio using Sphinx | Offline 
+try:
+    print("Sphinx thinks you said: " + r.recognize_sphinx(audio))
+except sr.UnknownValueError:
+    print("Sphinx could not understand audio file given")
+except sr.RequestError as e:
+    print("Sphinx error:  {0}".format(e))
